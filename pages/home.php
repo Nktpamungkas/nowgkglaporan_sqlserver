7af1e73debad453ba51c208d90148812aac2a99f
dedenkurnia001 <dedenkurnia001@gmail.com>
2024-10-24 10:29:33 +0700

    menghilangkan link

--- pages/home.php
+++ pages/home.php
@@ -28,7 +28,7 @@
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="IdentifikasiKainGreige" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
