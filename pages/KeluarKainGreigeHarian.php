40cb8ad25cf28b908b4efb7e7f6093eadafd8a53
usman.as <usman.as@indotaichen.com>
2025-10-16 08:16:57 +0700

    tambahan jual BS fix

--- pages/KeluarKainGreigeHarian.php
+++ pages/KeluarKainGreigeHarian.php
@@ -48,11 +48,11 @@ $Akhir = isset($_POST['tgl_akhir']) ? $_POST['tgl_akhir'] : '';
         </div>
         
       <!-- <button class="btn btn-info" type="submit">Cari Data</button> -->
-      <form method="POST">
+<!--      <form method="POST">-->
         <button type="submit" name="submit" class="btn btn-success">
           <i class="icofont icofont-search-alt-1"></i> Cari data
         </button>
-      </form>
+<!--      </form>-->
 
       <?php
       include 'koneksi.php'; // koneksi ke SQL Server
@@ -94,6 +94,7 @@ $Akhir = isset($_POST['tgl_akhir']) ? $_POST['tgl_akhir'] : '';
       ?>
 
       </div>
+	</div>		  
 		<?php if ($Awal != "" and $Akhir != "") {?>
 		<div class="card card-warning">
               <div class="card-header">
@@ -782,6 +783,237 @@ SELECT CASE WHEN PROJECTCODE <> '' THEN PROJECTCODE ELSE ORIGDLVSALORDLINESALORD
 $no++;
         $totRol12 = $totRol12 + $rowdb21['JML'];
         $totKG12 = $totKG12 + $rowdb21['KG'];
+
+    }?>
+				  </tbody>
+     <tfoot>
+	<tr>
+	    <th style="text-align: center">&nbsp;</th>
+	    <th style="text-align: center">&nbsp;</th>
+	    <th style="text-align: left">&nbsp;</th>
+	    <th style="text-align: left">&nbsp;</th>
+	    <th style="text-align: center">&nbsp;</th>
+	    <th>&nbsp;</th>
+	    <th style="text-align: center"><span style="text-align: left">Total</span></th>
+	    <th style="text-align: center"><?php echo $totRol12; ?></th>
+	    <th style="text-align: right"><?php echo number_format(round($totKG12, 2), 2); ?></th>
+	    <th style="text-align: center">&nbsp;</th>
+	    <th style="text-align: center">&nbsp;</th>
+	    <th style="text-align: center">&nbsp;</th>
+	    </tr>
+	 </tfoot>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+	<div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Detail Laporan Jual BS</h3>
+
+          </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example5" class="table table-sm table-bordered table-striped" style="font-size: 13px; text-align: center;">
+                  <thead>
+                  <tr>
+                    <th valign="middle" style="text-align: center">No</th>
+                    <th valign="middle" style="text-align: center">TglKeluar</th>
+                    <th valign="middle" style="text-align: center">Buyer</th>
+                    <th valign="middle" style="text-align: center">Customer</th>
+                    <th valign="middle" style="text-align: center">ProjectCode</th>
+                    <th valign="middle" style="text-align: center">Code</th>
+                    <th valign="middle" style="text-align: center">LOT</th>
+                    <th valign="middle" style="text-align: center">Qty</th>
+                    <th valign="middle" style="text-align: center">Berat/Kg</th>
+                    <th valign="middle" style="text-align: center">Note</th>
+                    <th valign="middle" style="text-align: center">Project Awal</th>
+                    <th valign="middle" style="text-align: center">User</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+				  <?php
+
+    $no = 1;
+    $c = 0;
+
+    $sqlDB21 = " 
+	SELECT
+	s.TRANSACTIONDATE,
+	SUM(s.BASEPRIMARYQUANTITY) AS QTY_KG,
+	COUNT(s.BASEPRIMARYQUANTITY) AS QTY_ROL,
+	SUM(s.BASESECONDARYQUANTITY) AS QTY_CONES,
+	s.LOTCODE,
+	s.ORDERCODE,
+	s.ORDERLINE,
+	s.DECOSUBCODE01,
+	s.DECOSUBCODE02,
+	s.DECOSUBCODE03,
+	s.DECOSUBCODE04,
+	s.DECOSUBCODE05,
+	s.DECOSUBCODE06,
+	s.DECOSUBCODE07,
+	s.DECOSUBCODE08,
+	s.WHSLOCATIONWAREHOUSEZONECODE,
+	s.WAREHOUSELOCATIONCODE,
+	s.CREATIONUSER, 
+	sl.ITEMDESCRIPTION,
+	sl.EXTERNALREFERENCE,
+	sd.GOODSISSUEDATE,
+	f.SUMMARIZEDDESCRIPTION,
+	b.LEGALNAME1
+FROM
+	STOCKTRANSACTION s
+LEFT OUTER JOIN SALESDOCUMENTLINE sl ON
+	sl.SALESDOCUMENTPROVISIONALCODE = s.ORDERCODE
+	AND sl.ORDERLINE = s.ORDERLINE
+LEFT OUTER JOIN SALESDOCUMENT sd ON
+	sl.SALESDOCUMENTPROVISIONALCODE = sd.PROVISIONALCODE 
+	AND sl.SALESDOCUMENTCOMPANYCODE = sd.COMPANYCODE  
+	AND sl.SALDOCPROVISIONALCOUNTERCODE = sd.PROVISIONALCOUNTERCODE	
+LEFT OUTER JOIN FULLITEMKEYDECODER f ON
+	s.FULLITEMIDENTIFIER = f.IDENTIFIER
+LEFT OUTER JOIN LOT lt ON
+	s.LOTCODE =lt.CODE AND 
+	s.COMPANYCODE = '100' AND
+	s.ITEMTYPECODE = lt.ITEMTYPECODE AND
+	s.DECOSUBCODE01 = lt.DECOSUBCODE01 AND
+	s.DECOSUBCODE02 = lt.DECOSUBCODE02 AND
+	s.DECOSUBCODE03 = lt.DECOSUBCODE03 AND
+	s.DECOSUBCODE04 = lt.DECOSUBCODE04 AND
+	s.DECOSUBCODE05 = lt.DECOSUBCODE05 AND
+	s.DECOSUBCODE06 = lt.DECOSUBCODE06 AND
+	s.DECOSUBCODE07 = lt.DECOSUBCODE07 AND
+	s.DECOSUBCODE08 = lt.DECOSUBCODE08  
+LEFT OUTER JOIN CUSTOMERSUPPLIERDATA cs ON
+	cs.COMPANYCODE ='100' AND
+	cs.TYPE ='2' AND
+	cs.CODE = lt.SUPPLIERCODE 
+LEFT OUTER JOIN BUSINESSPARTNER b ON
+	b.NUMBERID = cs.BUSINESSPARTNERNUMBERID 
+WHERE
+	s.ITEMTYPECODE = 'KGF'
+	AND s.TEMPLATECODE = '304'
+	AND NOT s.LOTCODE LIKE '%JUAL%'
+	AND s.LOGICALWAREHOUSECODE = 'M034'
+	AND s.TRANSACTIONDATE BETWEEN '$Awal' AND '$Akhir'
+GROUP BY
+	s.TRANSACTIONDATE,
+	s.LOTCODE,
+	s.ORDERCODE,
+	s.ORDERLINE,
+	s.DECOSUBCODE01,
+	s.DECOSUBCODE02,
+	s.DECOSUBCODE03,
+	s.DECOSUBCODE04,
+	s.DECOSUBCODE05,
+	s.DECOSUBCODE06,
+	s.DECOSUBCODE07,
+	s.DECOSUBCODE08,
+	s.WHSLOCATIONWAREHOUSEZONECODE,
+	s.WAREHOUSELOCATIONCODE,
+	s.CREATIONUSER, 
+	sl.ITEMDESCRIPTION,
+	sl.EXTERNALREFERENCE,
+	sd.GOODSISSUEDATE,
+	f.SUMMARIZEDDESCRIPTION,
+	b.LEGALNAME1
+	";
+    $stmt1 = db2_exec($conn1, $sqlDB21, array('cursor' => DB2_SCROLLABLE));
+    //}
+    while ($rowdb21 = db2_fetch_assoc($stmt1)) {
+        if ($rowdb21['LOGICALWAREHOUSECODE'] == 'M501') {$knitt = 'LT2';} else if ($rowdb21['LOGICALWAREHOUSECODE'] = 'P501') {$knitt = 'LT1';}
+        if ($rowdb21['PROJECTCODE'] != "") {$project = $rowdb21['PROJECTCODE'];} else { $project = $rowdb21['ORIGDLVSALORDLINESALORDERCODE'];}
+        $kdbenang = trim($rowdb21['DECOSUBCODE01']) . " " . trim($rowdb21['DECOSUBCODE02']) . " " . trim($rowdb21['DECOSUBCODE03']) . " " . trim($rowdb21['DECOSUBCODE04']) . " " . trim($rowdb21['DECOSUBCODE05']) . " " . trim($rowdb21['DECOSUBCODE06']) . " " . trim($rowdb21['DECOSUBCODE07']) . " " . trim($rowdb21['DECOSUBCODE08']);
+        $sqlDB22 = " SELECT SALESORDER.CODE, SALESORDER.EXTERNALREFERENCE, SALESORDER.ORDPRNCUSTOMERSUPPLIERCODE,
+		ITXVIEWAKJ.LEGALNAME1, ITXVIEWAKJ.ORDERPARTNERBRANDCODE, ITXVIEWAKJ.LONGDESCRIPTION
+		FROM DB2ADMIN.SALESORDER SALESORDER LEFT OUTER JOIN DB2ADMIN.ITXVIEWAKJ
+       	ITXVIEWAKJ ON SALESORDER.CODE=ITXVIEWAKJ.CODE
+		WHERE SALESORDER.CODE='$project' ";
+        $stmt2 = db2_exec($conn1, $sqlDB22, array('cursor' => DB2_SCROLLABLE));
+        $rowdb22 = db2_fetch_assoc($stmt2);
+        if (strlen(trim($rowdb21['LOTCODE'])) == "8") {$Wlot = " AND ( p.PROJECTCODE ='" . trim($rowdb21['PROJAWAL']) . "' OR p.ORIGDLVSALORDLINESALORDERCODE ='" . trim($rowdb21['PROJAWAL']) . "' OR p.CODE='" . trim($rowdb21['LOTCODE']) . "' ) ";} else { $Wlot = " AND ( p.PROJECTCODE ='" . trim($project) . "' OR p.ORIGDLVSALORDLINESALORDERCODE ='" . trim($project) . "' ) ";}
+
+        $sqlDB23 = " SELECT p.SUBCODE01,p.SUBCODE02,p.SUBCODE03,p.SUBCODE04,p.SUBCODE05,p.SUBCODE06,p.SUBCODE07, p.LONGDESCRIPTION FROM (
+SELECT p2.ITEMTYPEAFICODE,p2.SUBCODE01,p2.SUBCODE02,p2.SUBCODE03,p2.SUBCODE04,
+p2.SUBCODE05,p2.SUBCODE06,p2.SUBCODE07  FROM PRODUCTIONDEMAND p
+LEFT OUTER JOIN PRODUCTIONRESERVATION p2 ON p.CODE =p2.ORDERCODE
+WHERE p.ITEMTYPEAFICODE ='KGF' AND p.SUBCODE01='" . trim($rowdb21['DECOSUBCODE01']) . "'
+AND p.SUBCODE02 ='" . trim($rowdb21['DECOSUBCODE02']) . "' AND p.SUBCODE03 ='" . trim($rowdb21['DECOSUBCODE03']) . "' AND
+p.SUBCODE04='" . trim($rowdb21['DECOSUBCODE04']) . "' $Wlot
+) a LEFT OUTER JOIN PRODUCT p ON
+p.ITEMTYPECODE ='GYR' AND
+p.SUBCODE01= a.SUBCODE01 AND p.SUBCODE02= a.SUBCODE02 AND
+p.SUBCODE03= a.SUBCODE03 AND p.SUBCODE04= a.SUBCODE04 AND
+p.SUBCODE05= a.SUBCODE05 AND p.SUBCODE06= a.SUBCODE06 AND
+p.SUBCODE07= a.SUBCODE07
+GROUP BY
+p.SUBCODE01,p.SUBCODE02,
+p.SUBCODE03,p.SUBCODE04,
+p.SUBCODE05,p.SUBCODE06,
+p.SUBCODE07,p.LONGDESCRIPTION ";
+        $stmt3 = db2_exec($conn1, $sqlDB23, array('cursor' => DB2_SCROLLABLE));
+        $ai = 0;
+        $a[0] = "";
+        $a[1] = "";
+        $a[2] = "";
+        $a[3] = "";
+        while ($rowdb23 = db2_fetch_assoc($stmt3)) {
+            $a[$ai] = $rowdb23['LONGDESCRIPTION'];
+            $ai++;
+        }
+        $sqlDB24 = "
+SELECT pr.LONGDESCRIPTION,p.PRODUCTIONORDERCODE,pd.SUBCODE01,pd.SUBCODE02,pd.SUBCODE03,
+	pd.SUBCODE04,pd.SUBCODE05,pd.SUBCODE06,pd.SUBCODE07,pd.SUBCODE08,pd.INTERNALREFERENCE,
+	LISTAGG(DISTINCT  TRIM(pd.CODE),', ') AS PRODUCTIONDEMANDCODE
+	FROM PRODUCTIONDEMANDSTEP p
+	LEFT OUTER JOIN PRODUCTIONDEMAND pd ON pd.CODE =p.PRODUCTIONDEMANDCODE
+	LEFT JOIN PRODUCT pr ON
+    pr.ITEMTYPECODE = pd.ITEMTYPEAFICODE
+    AND pr.SUBCODE01 = pd.SUBCODE01
+    AND pr.SUBCODE02 = pd.SUBCODE02
+    AND pr.SUBCODE03 = pd.SUBCODE03
+    AND pr.SUBCODE04 = pd.SUBCODE04
+    AND pr.SUBCODE05 = pd.SUBCODE05
+    AND pr.SUBCODE06 = pd.SUBCODE06
+    AND pr.SUBCODE07 = pd.SUBCODE07
+    AND pr.SUBCODE08 = pd.SUBCODE08
+    AND pr.SUBCODE09 = pd.SUBCODE09
+    AND pr.SUBCODE10 = pd.SUBCODE10
+WHERE (pd.PROJECTCODE ='$project' OR pd.ORIGDLVSALORDLINESALORDERCODE ='$project') AND p.PRODUCTIONORDERCODE='$rowdb21[ORDERCODE]'
+	GROUP BY pr.LONGDESCRIPTION,p.PRODUCTIONORDERCODE,pd.SUBCODE01,pd.SUBCODE02,pd.SUBCODE03,
+	pd.SUBCODE04,pd.SUBCODE05,pd.SUBCODE06,pd.SUBCODE07,pd.SUBCODE08,pd.INTERNALREFERENCE ";
+        $stmt4 = db2_exec($conn1, $sqlDB24, array('cursor' => DB2_SCROLLABLE));
+        $rowdb24 = db2_fetch_assoc($stmt4);
+
+        $sqlDB25 = "
+SELECT CASE WHEN PROJECTCODE <> '' THEN PROJECTCODE ELSE ORIGDLVSALORDLINESALORDERCODE  END  AS PROJECT FROM PRODUCTIONDEMAND WHERE CODE='" . $rowdb21['LOTCODE'] . "'
+";
+        $stmt5 = db2_exec($conn1, $sqlDB25, array('cursor' => DB2_SCROLLABLE));
+        $rowdb25 = db2_fetch_assoc($stmt5);
+
+        if ($rowdb22['LEGALNAME1'] == "") {$langganan = "";} else { $langganan = $rowdb22['LEGALNAME1'];}
+        if ($rowdb22['ORDERPARTNERBRANDCODE'] == "") {$buyer = "";} else { $buyer = $rowdb22['ORDERPARTNERBRANDCODE'];}
+
+        ?>
+	  <tr>
+	  <td style="text-align: center"><?php echo $no; ?></td>
+	  <td style="text-align: center"><?php echo $rowdb21['TRANSACTIONDATE']; ?></td>
+	  <td style="text-align: left"><?php echo $buyer; ?></td>
+	  <td style="text-align: left"><?php echo $langganan; ?></td>
+	  <td style="text-align: center"><?php echo $project; ?></td>
+	  <td><?php echo $kdbenang; ?></td>
+      <td style="text-align: center"><?php echo $rowdb21['LOTCODE']; ?></td>
+      <td style="text-align: center"><?php echo $rowdb21['QTY_ROL']; ?></td>
+      <td style="text-align: right"><?php echo number_format(round($rowdb21['QTY_KG'], 2), 2); ?></td>
+      <td style="text-align: left"><?php echo $rowdb21['NOTE'];if ($rowdb21['PTG'] == "1") {echo " <span class='btn btn-xs btn-danger'>Permintan Potong</span>";} else if ($rowdb21['PTG'] == "2") {echo " <span class='btn btn-xs btn-warning'>Tarikan Kain</spin>";} ?></span></td>
+      <td style="text-align: center"><?php if ($rowdb21['PROJAWAL'] != "") {echo $rowdb21['PROJAWAL'];} else if ($rowdb25['PROJECT'] != "") {echo $rowdb25['PROJECT'];} else {echo $rowdb24['INTERNALREFERENCE'];}?></td>
+      <td style="text-align: center"><?php echo $rowdb21['CREATIONUSER']; ?></td>
+      </tr>
+
+	<?php
+$no++;
+        $totRol12 = $totRol12 + $rowdb21['QTY_ROL'];
+        $totKG12 = $totKG12 + $rowdb21['QTY_KG'];
 
     }?>
 				  </tbody>
